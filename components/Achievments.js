3234d018e2d8a0ccd91bdbddde2dfb2563195fc5
suyashsethia <suyash.sethia0848@gmail.com>
2023-01-17 11:58:22 +0530

    added home.js

--- components/Achievments.js
+++ components/Achievments.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Achievments = () => {
     return (
 
-        <section className="text-gray-600 body-font">
+        <section className="text-gray-600 body-font ">
             <div className="container px-5 py-24 mx-auto">
-                <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+                <div className="flex items-center lg:w-4/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
                     <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
                             <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
@@ -21,7 +21,7 @@ const Achievments = () => {
                         </a>
                     </div>
                 </div>
-                <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+                <div className="flex items-center lg:w-4/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
                     <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
                         <h2 className="text-gray-900 text-lg title-font font-medium mb-2">The Catalyzer</h2>
                         <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
@@ -39,7 +39,7 @@ const Achievments = () => {
                         </svg>
                     </div>
                 </div>
-                <div className="flex items-center lg:w-3/5 mx-auto sm:flex-row flex-col">
+                <div className="flex items-center lg:w-4/5 mx-auto sm:flex-row flex-col">
                     <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
                             <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,13 @@ import { motion, useScroll } from "framer-motion"
 import Achievments from '@/components/Achievments'
 import Education from '@/components/Education'
 import ContactUs from '@/components/ContactUs'
+import { MyComponent } from '@/components/Vanta_cells'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const { scrollYProgress } = useScroll();
-
+  const Van = 'fuck';
   return (
     <>
       <Head>
@@ -23,7 +24,7 @@ export default function Home() {
       </Head>
 
       <section className="text-gray-600 body-font h-fu">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div id={Van} className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Dr. Ashmeet Choudhary
               <br className="hidden lg:inline-block text-sm" />(MBBS , MD , DM (Gastroentrologist, AIIMS-Delhi))
@@ -39,10 +40,12 @@ export default function Home() {
           </div>
         </div>
       </section>
+      {/* <Home></Home> */}
       <Education></Education>
       <Achievments></Achievments>
       <Testimonials />
-      <ContactUs/>
+      <div id="fuck"></div>
+      <ContactUs />
     </>
 
   )
